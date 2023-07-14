82bf06cdf555f3f5cd148a5895283739abd762e8
Laila Hasmi <lhasmi@2-A-1.42heilbronn.de>
2023-07-15 00:05:16 +0200

    usleep&&routine_done

--- Philo/philo.h
+++ Philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: lhasmi <lhasmi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 20:15:50 by lhasmi            #+#    #+#             */
-/*   Updated: 2023/07/13 22:04:49 by lhasmi           ###   ########.fr       */
+/*   Updated: 2023/07/14 23:29:32 by lhasmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,34 @@
 # include <unistd.h>
 # include <stdbool.h>
 
-
-typedef struct s_philosophers[]
+// there is no function to check if a philosopher died).
+// Also, this code doesn't implement a key part of the Dining Philosophers
+// problem: the issue of starvation (a philosopher might never get to eat).
+typedef struct s_philosophers
 {
 	int				id;
-	long long		time_to_die;
 	long long		last_time_to_eat;
-	long long		time_to_sleep;
-	int				meals_required;
 	pthread_t		thread;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
-}					t_philosophers;
+	t_data			*data;
+} t_philosophers;
 
 typedef struct s_data
 {
-	t_philosophers		**philos;
-	bool  				dead;
-	// pthread_mutex_t		print;
-	// pthread_mutex_t		death;
-	// pthread_mutex_t		*fork;
+	t_philosophers		*philos;
+	bool				dead;
+	long long			time_to_die;
+	long long			time_to_eat;
+	long long			time_to_sleep;
+	int					meals_required;
+	long long			start;
+	pthread_mutex_t		write;
 }				t_data;
 
 void				*routine(void *arg);
+void				ft_usleep(long time);
+long				is_timenow(void);
+void	printing(t_philosophers *philosopher, char *msg);
 
 #endif
--- Philo/threads.c
+++ Philo/threads.c
@@ -6,7 +6,7 @@
 /*   By: lhasmi <lhasmi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 20:00:27 by lhasmi            #+#    #+#             */
-/*   Updated: 2023/07/13 22:06:51 by lhasmi           ###   ########.fr       */
+/*   Updated: 2023/07/14 23:37:42 by lhasmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,29 +29,76 @@ long	is_timenow(void)
 	return (timenow.tv_sec * 1000 + timenow.tv_usec / 1000);
 }
 
+void	ft_usleep(long time)
+{
+	long start_time;
+
+	start_time = is_timenow();
+	while (is_timenow() - start_time < time)
+		usleep(time);
+}
+
+void	printing(t_philosophers *philosopher, char *msg)
+{
+	pthread_mutex_lock(&philosopher->data->write);
+	if (!philosopher->data->dead)
+	{
+		printf("%ld Philosopher %d %s\n",
+			is_timenow() - philosopher->data->start,
+			philosopher->id, msg);
+	}
+	pthread_mutex_unlock(&philosopher->data->write);
+}
+
 void	*routine(void *arg)
 {
-	t_philosopher	*philosopher;
+	t_philosophers	*philosophers;
 
-	philosopher = (t_philosopher *)arg;
-	while (philosopher->meals_eaten != philosopher->meals_required)
+	philosophers = (t_philosophers *)arg;
+	while (1)
 	{
 		// check flag data.dead
-		// print eating (print function has calls is_timenow)
-		// call usleep
-		// check flag data.dead
-		// print sleeping or thinking (print function has calls is_timenow)
-		// call usleep
+		if (philosophers->data->dead)
+			break;
 
-	}
+		// Philosopher is eating
+		pthread_mutex_lock(philosophers->left_fork);
+		pthread_mutex_lock(philosophers->right_fork);
+		// print eating (print function calls is_timenow)
+		printing(philosophers, "eating");
+		philosophers->last_time_to_eat = get_time(); // Assume get_time is a function that gets current time
+		// call ft_usleep
+		ft_usleep(philosophers->data->time_to_eat);
+		pthread_mutex_unlock(philosophers->left_fork);
+		pthread_mutex_unlock(philosophers->right_fork);
+
+		// check flag data.dead
+		if (philosophers->data->dead)
+			break;
+
+		// print sleeping or thinking (print function calls is_timenow)
+		printing(philosophers, "is sleeping");
+		ft_usleep(philosophers->data->time_to_sleep);
+
+		// check flag data.dead
+		if (philosophers->data->dead)
+			break;
+
+		// Philosopher is thinking
+		printing(philosophers, "is thinking");	}
 	return NULL;
 }
 
 int	main(int argc, char *argv[])
 {
+	t_data data;
 	int num_philosophers = atoi(argv[1]);
 	pthread_mutex_t	forks[num_philosophers];
-	t_philosopher	philosophers[num_philosophers];
+	t_philosophers	*philosophers = (t_philosophers *)malloc(sizeof(t_philosophers) * num_philosophers);
+	if (philosophers == NULL)
+	{
+		// handle the error
+	}
 	int i = 0;
 	if (argc < 5)
 	{
@@ -62,103 +109,44 @@ int	main(int argc, char *argv[])
 	while ( i < num_philosophers)
 	{
 		pthread_mutex_init(&forks[i], NULL);
-		++i;
+		i++;
 	}
+
+	pthread_mutex_init(&data.write, NULL);
+
+	data.start = is_timenow(); // Set start_time for data.
+
 	i = 0;
 	while (i < num_philosophers)
 	{
 		philosophers[i].id = i;
-		philosophers[i].time_to_die = atoi(argv[2]);
-		philosophers[i].time_to_eat = atoi(argv[3]);
-		philosophers[i].time_to_sleep = atoi(argv[4]);
-		philosophers[i].meals_required = (argc == 6) ? atoi(argv[5]) : -1;
+		data.time_to_die = atoll(argv[2]);
+		data.time_to_eat = atoll(argv[3]);
+		data.time_to_sleep = atoll(argv[4]);
+		data.meals_required = (argc == 6) ? atoll(argv[5]) : -1;
 		philosophers[i].left_fork = &forks[i];
 		philosophers[i].right_fork = &forks[(i + 1) % num_philosophers];
+		philosophers[i].data = &data; // Link data to each philosopher.
 		if (pthread_create(&philosophers[i].thread, NULL, &routine,
 				&philosophers[i]) != 0)
 			return (2);
-		++i;
+		i++;
 	}
 	i = 0;
 	while (i < num_philosophers)
 	{
 		if (pthread_join(philosophers[i].thread, NULL) != 0)
 			return (3);
-		++i;
+		i++;
 	}
 	i = 0;
 	while (i < num_philosophers)
 	{
 		pthread_mutex_destroy(&forks[i]);
-		++i;
+		i++;
 	}
+	pthread_mutex_destroy(&data.write);
+
+	free(philosophers);
 	return (0);
 }
-
-// int	main(int argc, char *argv[])
-// {
-// 	int	number_of_philosophers;
-// 	t_philosopher
-
-// 	if (argc < 5)
-// 	{
-// 		printf("Arguments: number_of_philosophers, time_to_die, time_to_eat,
-// 				time_to_sleep, number_of_times_each_philosopher_must_eat\n");
-// 		return (1);
-// 	}
-// 	pthread_mutex_init(&mutex, NULL);
-// 	if (pthread_create(&p1, NULL, &routine, NULL) != 0)
-// 		return (1);
-// 	if (pthread_create(&p2, NULL, &routine, NULL) != 0)
-// 		return (2);
-// 	// if (pthread_create(&p3, NULL, &routine, NULL) != 0)
-// 	//     return (3);
-// 	// if (pthread_create(&p4, NULL, &routine, NULL) != 0)
-// 	//     return (4);
-// 	if (pthread_join(p1, NULL) != 0)
-// 		return (5);
-// 	if (pthread_join(p2, NULL) != 0)
-// 		return (6);
-// 	// if (pthread_join(p3, NULL) != 0)
-// 	//     return (7);
-// 	// if (pthread_join(p4, NULL) != 0)
-// 	//     return (8);
-// 	pthread_mutex_destroy(&mutex);
-// 	printf("Number of mails: %d\n", mails);
-// 	return (0);
-// }
-
-//in a loop
-
-// int mails = 0;
-// pthread_mutex_t mutex;
-
-// void* routine() {
-//     for (int i = 0; i < 10000000; i++) {
-//         pthread_mutex_lock(&mutex);
-//         mails++;
-//         pthread_mutex_unlock(&mutex);
-//     }
-// }
-
-// int main(int argc, char* argv[]) {
-//     pthread_t th[8];
-//     int i;
-//     pthread_mutex_init(&mutex, NULL);
-//     for (i = 0; i < 8; i++) {
-//         if (pthread_create(th + i, NULL, &routine, NULL) != 0) {
-//             perror("Failed to create thread");
-//             return (1);
-//         }
-//         printf("Thread %d has started\n", i);
-//     }
-//     for (i = 0; i < 8; i++) {
-//         if (pthread_join(th[i], NULL) != 0) {
-//             return (2);
-//         }
-//         printf("Thread %d has finished execution\n", i);
-//     }
-//     pthread_mutex_destroy(&mutex);
-//     printf("Number of mails: %d\n", mails);
-//     return (0);
-// }
